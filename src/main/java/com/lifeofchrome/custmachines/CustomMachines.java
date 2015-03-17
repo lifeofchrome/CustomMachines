f7e3f158c0a9e965ea38a628283e93d0aeb821ab
lifeofchrome <lifeofchrome@gmail.com>
2015-03-16 20:12:12 -0500

    Added initial code
    Added gradle build, mcmod.info, basic mod class

---
+++ src/main/java/com/lifeofchrome/custmachines/CustomMachines.java
@@ -0,0 +1,33 @@
+package com.lifeofchrome.custmachines;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="CustomMachines", name="Custom Machines", version="1.7.10-1.0")
+public class CustomMachines
+{
+    @Mod.Instance("CustomMachines")
+    public static CustomMachines instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
+
+//    Left off: https://youtu.be/oVq5kslZJc4?t=2m30s
